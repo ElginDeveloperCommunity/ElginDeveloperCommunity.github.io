17e1bcd75e13ae94a81c702961443f70cbf246e9
felipm <fdepaulamoreira@gmail.com>
2024-10-17 08:36:03 -0300

    Ajustes versão 3.13.8
    Ajustes versão 3.13.8

---
+++ group__g11.js
@@ -0,0 +1,4 @@
+var group__g11 =
+[
+    [ "Funcoes NFC-e E1", "group__g50.html", "group__g50" ]
+];
--- modules.js
+++ modules.js
@@ -2,6 +2,8 @@ var modules =
 [
     [ "Impressoras", "group__g0.html", "group__g0" ],
     [ "SAT", "group__g2.html", "group__g2" ],
+    [ "E1_Bridge", "group__g12.html", "group__g12" ],
+    [ "E1 NFC-e", "group__g11.html", "group__g11" ],
     [ "Balança", "group__g5.html", "group__g5" ],
     [ "Poste Sinalizador", "group__g10.html", "group__g10" ],
     [ "Gerador PDF", "group__g9.html", "group__g9" ],
